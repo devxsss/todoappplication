bf8fdbb93e16480578b9ac08556179a63288c527
Maria Devina <devinamuljono@live.com>
2022-01-05 14:25:55 +0900

    headerを作った

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header">
+    <div class="appWrapper">
+      <span>Todo App</span>
+    </div>
+    <div class="addNew">
+      <b-icon
+        icon="plus-circle"
+        scale="2"
+        style="color: #7952b3"
+        @click="goToAddNewTask()"
+      >
+      </b-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToAddNewTask() {
+      this.$router.push(`./todo`);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import BootstrapVue from 'bootstrap-vue'; // add
+import 'bootstrap/dist/css/bootstrap.css'; // add
+import 'bootstrap-vue/dist/bootstrap-vue.css'; // add
+import { BootstrapVueIcons } from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons); // add
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
