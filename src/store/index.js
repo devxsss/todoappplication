c2964c28dbe608abc9239dcee6c3cebe846b4458
いとうけいた <keita.itou2@grp.tmnf.jp>
2022-01-05 17:27:53 +0900

    storeの変更

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,32 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    taskName:'',
+    taskDate:'',
+    taskImportance:'',
+  },
+  mutations: {
+    setTaskName(state,taskName){
+      state.taskName= taskName
+    },
+    setTaskDate(state,taskDate){
+      state.taskDame= taskDate
+    },
+    setTaskName(state,taskImportance){
+      state.taskImportance= taskImportance
+    }
+  },
+  actions: {
+    setTaskName(state,taskName){
+      state.taskName= taskName
+    },
+    setTaskDate(state,taskDate){
+      state.taskDame= taskDate
+    },
+    setTaskName(state,taskImportance){
+      state.taskImportance= taskImportance
+    }
+  },
   modules: {},
 });
